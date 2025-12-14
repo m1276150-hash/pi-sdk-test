aef470641e674e47e1305c285b3e3d88a1219ac1
경오 <you@example.com>
2025-12-14 16:57:37 +0900

    Add logo to public folder

--- app/page.js
+++ app/page.js
@@ -1,3 +1,17 @@
 export default function Home() {
   return <h1>Hello Xpaio!</h1>
+
+  // app/layout.js
+export const metadata = {
+  title: 'Xpaio',
+  description: 'Next.js project on Netlify',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
 }
